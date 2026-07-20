b2a7a7e19925f9673642e227ec727fa579787d6b
paksi <240254790+muhammad-paksi@users.noreply.github.com>
2026-07-20 08:47:34 +0700

    Update proxy for visit to dashboard page
    - Normalize spaces to underscores in pathname and search query parameters.
    - Update floor routing to support multiple valid floors (lantai_6, lantai_7, lantai_8) and only redirect when accessing invalid floor paths.
    
    Signed-off-by: paksi <240254790+muhammad-paksi@users.noreply.github.com>

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,18 +2,24 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export async function proxy(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+  let { pathname, search } = request.nextUrl;
+
+  // Ubah %20 atau spasi dalam pathname dan search query menjadi underscore "_".
+  pathname = pathname.replace(/%20| /g, '_');
+  search = search.replace(/%20| /g, '_');
+
+  const trailingPaths = pathname.split('/');
 
   // Jika path TEPAT HANYA '/dasbor' atau '/dasbor/'
-  if (pathname === '/dasbor' || pathname === '/dasbor/') {
-    // Redirect ke /dasbor/bla
-    return NextResponse.redirect(new URL('/dasbor/lantai_6', request.url));
+  if (trailingPaths[1] === 'dasbor') {
+    const lantai = trailingPaths[2];
+    if (lantai !== "lantai_6" && lantai !== "lantai_7" && lantai !== "lantai_8"){
+      // Redirect ke /dasbor/bla
+      return NextResponse.redirect(new URL('/dasbor/lantai_6', request.url));
+    }
   }
 
-  // Jika ada ekornya (misal: /dasbor/profile), biarkan lewat
   return NextResponse.next();
-  
-  // return NextResponse.redirect(new URL('/home', request.url))
 }
 
 export const config = {
